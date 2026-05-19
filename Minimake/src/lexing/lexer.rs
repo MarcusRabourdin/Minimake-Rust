885fe63e3276df5d39b8b32e6dc1bfb585054c3a
Marcus Rabourdin <rabourdinmarcus@gmail.com>
2026-05-20 00:54:01 +0200

    lexer: removed deadcode

--- Minimake/src/lexing/lexer.rs
+++ Minimake/src/lexing/lexer.rs
@@ -2,28 +2,6 @@ use std::io;
 use std::fs::File;
 use std::path::Path;
 use std::io::{BufRead};
-use std::collections::LinkedList;
-
-enum Type {
-    Variable(String),
-    Rule(String),
-    Command(String),
-}
-
-pub struct Node {
-    LinkedList<String> : node,
-}
-
-pub struct Lexer {
-    
-}
-
-fn create_lexer() {
-    let lexer = Lexer {
-    
-    }
-}
-
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
    where P: AsRef<Path>, {
@@ -35,8 +13,6 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 
 
 pub fn lex(file: &str) {
-    let lexer = Lexer {};
-
 
     if let Ok(lines) = read_lines(file) {
         for line in lines.map_while(Result::ok)
