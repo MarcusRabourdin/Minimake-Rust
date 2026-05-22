4f1614e74d5e3a5e498e5b71d5d2ed5adaa0f3f6
Marcus Rabourdin <rabourdinmarcus@gmail.com>
2026-05-22 18:34:18 +0200

    parser: added some methods definition

--- Minimake/src/parsing/parser.rs
+++ Minimake/src/parsing/parser.rs
@@ -1,1 +1,35 @@
+use std::collections::HashMap;
 
+use crate::lexing::lexer::Token;
+
+pub struct Parser {
+    dict : HashMap<Token,String>,
+}
+
+impl Parser {
+
+    fn add_element(&self, token : &Token)
+    {
+    
+    }
+
+    pub fn parse_rule(&self, line : &String)
+    {
+        
+    }
+
+    fn parse_def(&self, line : &String)
+    {
+
+    }
+
+    pub fn parse_variable(&self, line : &String)
+    {
+
+    }
+
+    pub fn parse_command(&self, line : &String)
+    {
+
+    }
+}
