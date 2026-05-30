6842cf46b4e8525d39964ba7434f18470dbc1f62
Marcus Rabourdin <rabourdinmarcus@gmail.com>
2026-05-30 02:21:34 +0200

    parser: parse() function

--- Minimake/src/parsing/parser.rs
+++ Minimake/src/parsing/parser.rs
@@ -14,6 +14,7 @@ struct Variable {
 pub struct Parser {
     rules : Vec<Rule>,
     variables : Vec<Variable>,
+    in_rule : bool,
 
 }
 
@@ -21,17 +22,28 @@ fn parser_create() -> Parser {
     Parser {
         rules : Vec::new(),
         variables : Vec::new(),
+        in_rule : false,
     }
 }
 
 
 pub fn parse(lexer : &Lexer) -> Parser {
     let mut parser = parser_create();
-    lexer.token_list.iter().for_each(|token| {
-    
-
-    }); 
-
-    parser
-    
+    let list = lexer.token_list;
+    match list {
+        None => panic!("there is no token_list"),
+        Some(l) => {
+             for element in l {
+                match element {
+                    Token::Variable(var) => { parser.add_variable(var); },
+                    Token::Rule(target,dep ) => { parser.add_rule(target,dep); },
+                    Token::Recipe(recipe) => { parser.add_recipe(recipe); },
+                    Token::Other(other) => {},
+                }
+            }
+        }
+        
+    }
+    parser    
 }
+
